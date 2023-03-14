6a213a9ce473fabccd0e01c61c0273700cdd5cd6
Simo Ryu <cloneofsimo@gmail.com>
2023-03-14 19:05:57 +0900

    stubgen

--- cbuild.sh
+++ cbuild.sh
@@ -1,4 +1,5 @@
 cmake -S . -B build
 cmake --build build
-pip install .
 pybind11-stubgen rg2.bin --no-setup --bare-numpy-ndarray
+mv stubs/rg2/bin-stubs/_rg2/__init__.pyi rg2/bin/__init__.pyi
+pip install .
